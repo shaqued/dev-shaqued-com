bbc684104f2ebec6a5302b24ee5bb48cfc7e3ec1
shaqued <shaqued201@gmail.com>
2021-02-15 09:21:23 +0200

    general working mobile navbar

--- src/components/layout/mobile-navbar/index.js
+++ src/components/layout/mobile-navbar/index.js
@@ -1,28 +1,40 @@
 import React, { useState } from 'react'
 import { GiHamburgerMenu } from 'react-icons/gi'
-import { IconContext } from 'react-icons'
+import { GrClose } from 'react-icons/gr'
 import { Link } from 'gatsby'
-import styles from '../header.module.css'
+import styles from './mobile-navbar.module.css'
 
 export default ({ buttons }) => {
   const [isOpen, setIsOpen] = useState(false)
 
   const handleClickOpen = () => {
+    console.log(isOpen)
     setIsOpen(!isOpen)
   }
 
   return (
     <div className={styles.mobileNavbar}>
-      <IconContext.Provider value={{ style: { fontSize: '30px' } }}>
-        <GiHamburgerMenu onClick={() => handleClickOpen()} />
-      </IconContext.Provider>
-      <div className={styles.mobiledrawer}>{isOpen ? 
-        buttons.map(button => (
-          <div key={button} className={styles.button}>
-            <Link to={`/${button}`}>{button}</Link>
-          </div>
-        )) : <div></div>}
-      </div>
+      <GiHamburgerMenu
+        className={styles.hamburger}
+        onClick={() => handleClickOpen()}
+      />
+
+      {isOpen ? (
+        <div className={styles.mobiledrawer}>
+          <GrClose
+            className={styles.closeButton}
+            onClick={() => handleClickOpen()}
+          />
+          
+          {buttons.map(button => (
+            <div key={button} className={styles.button}>
+              <Link to={`/${button}`}>{button}</Link>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <div></div>
+      )}
     </div>
   )
 }
