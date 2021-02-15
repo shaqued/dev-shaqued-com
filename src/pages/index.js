12052e6217b3284b8704fd360c5845ce3bc101d3
shaqued <shaqued201@gmail.com>
2021-02-16 00:01:01 +0200

    wip home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,26 @@
 import React from 'react'
 import Layout from '../components/layout'
+import profilePhoto from '../../static/circle-cropped.png'
+import styles from './index.module.css'
+import {Box, Typography} from '@material-ui/core'
 
 export default () => {
   return (
-      <Layout>Home Page!</Layout>
+    <Layout>
+      <Box className={styles.home}>
+        <img src={profilePhoto} className={styles.image} />
+        <Typography variant="h2" color="primary">
+          {'Hi, My Name is Shaqued'}
+        </Typography>
+        <Box className={styles.secondRow}>
+          <Typography variant="h2" color="secondary">
+            {"and i'm a "}
+          </Typography>
+          <Typography variant="h2" color="error">
+            &nbsp; {' software engineer'}
+          </Typography>
+        </Box>
+      </Box>
+    </Layout>
   )
 }
